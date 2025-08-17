14065565f23836dcf1ab71d88d93b032e90f3bff
Alina Garib <alinagrb@gmail.com>
2025-08-17 14:41:22 -0400

    commented auth

--- SpotifyAPI/spotifyAuth.py
+++ SpotifyAPI/spotifyAuth.py
@@ -1,45 +1,45 @@
-import requests
-import urllib.parse
-import base64
-import os
+# import requests
+# import urllib.parse
+# import base64
+# import os
 
-CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+# # use on local to get a new refresh token when needed
+# CLIENT_ID = ""
+# REDIRECT_URI = ""
 
-SCOPE = " ".join([
-    "user-read-playback-state",
-    "user-read-currently-playing",
-    "streaming",
-    "playlist-read-private",
-    "playlist-read-collaborative",
-    "user-library-read",
-    "user-read-private",
-    "user-top-read",
-    "user-read-recently-played",
-    "user-read-playback-position"
-])
-
-
-params = {
-    "client_id": CLIENT_ID,
-    "response_type": "code",
-    "redirect_uri": REDIRECT_URI,
-    "scope": SCOPE
-}
-print("Go to this URL and log in:\n")
-print("https://accounts.spotify.com/authorize?" + urllib.parse.urlencode(params))
+# SCOPE = " ".join([
+#     "user-read-playback-state",
+#     "user-read-currently-playing",
+#     "streaming",
+#     "playlist-read-private",
+#     "playlist-read-collaborative",
+#     "user-library-read",
+#     "user-read-private",
+#     "user-top-read",
+#     "user-read-recently-played",
+#     "user-read-playback-position"
+# ])
 
 
-AUTH_CODE = input("Enter the code from the URL: ")
+# params = {
+#     "client_id": CLIENT_ID,
+#     "response_type": "code",
+#     "redirect_uri": REDIRECT_URI,
+#     "scope": SCOPE
+# }
+# print("Go to this URL and log in:\n")
+# print("https://accounts.spotify.com/authorize?" + urllib.parse.urlencode(params))
 
-auth_header = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
-headers = {"Authorization": f"Basic {auth_header}"}
-data = {
-    "grant_type": "authorization_code",
-    "code": AUTH_CODE,
-    "redirect_uri": REDIRECT_URI
-}
 
-res = requests.post("https://accounts.spotify.com/api/token", data=data, headers=headers)
-print(res.json())
+# AUTH_CODE = input("Enter the code from the URL: ")
+
+# auth_header = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
+# headers = {"Authorization": f"Basic {auth_header}"}
+# data = {
+#     "grant_type": "authorization_code",
+#     "code": AUTH_CODE,
+#     "redirect_uri": REDIRECT_URI
+# }
+
+# res = requests.post("https://accounts.spotify.com/api/token", data=data, headers=headers)
+# print(res.json())
