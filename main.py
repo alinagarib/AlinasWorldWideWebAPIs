d6e950e31a498927aca239a9f6b7df1026f9fef8
Alina Garib <alinagrb@gmail.com>
2025-08-17 14:43:42 -0400

    removed auth app

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from WikiRaceAPI.fastestPath import app as wiki_race_app
-from SpotifyAPI.spotifyAuth import app as spotify_auth_app
 from SpotifyAPI.spotifyStats import app as spotify_stats_app
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 main_app = FastAPI()
 
@@ -15,9 +15,7 @@ main_app.add_middleware(
 )
 
 main_app.include_router(wiki_race_app.router, prefix="/wiki-race", tags=["WikiRace"])
-main_app.include_router(spotify_auth_app.router, prefix="/spotify-auth", tags=["SpotifyAuth"])
 main_app.include_router(spotify_stats_app.router, prefix="/spotify-stats", tags=["SpotifyStats"])
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(main_app, host="0.0.0.0", port=8000)
